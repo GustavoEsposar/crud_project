6c77133636c43618f18da36d95eaf20cb42bf8c2
guesposar <guesposar@icloud.com>
2024-01-17 21:26:06 -0300

    controllers and customers register

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 
 const router = require('express').Router();
 
+const CustomersController = require('../controllers/customers')
+
 // --------------------------->     rotes
 
 router.get('/', (req, res) => {
@@ -9,8 +11,16 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/register', (req, res) => {
+    res.render('register', {
+        title: 'Cadastro de Clientes'
+    })
+})
+
+router.post('/register/add', CustomersController.add)
+
 router.use((req, res) => {
-    res.send('Pagina não encontrada!')
+    res.send('Página não encontrada!')
 })
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public'))) 
 
 //enable server to receive data through POST (forms)
-app.set(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 
 //defining routes
 app.use('/', routes)
